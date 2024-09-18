1357ff1bfb8ae613de1b5de469263848de273178
Shivayogi21 <shivayogityapi2000@gmail.com>
2024-09-18 19:28:19 +0530

    About Us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-import Image from "next/image";
+
+import Hero from "@/Components/Hero/Hero";
+import OurMission from "@/Components/OurMission/Ourmission";
+
+import { AnimatedTooltipPreview } from "@/Components/Welcome/Welcome";
+import WhyChoose from "@/Components/Whychoese/Whychoese";
+
 
 export default function Home() {
   return (
-      <div>hi</div>
-  );
+  <>
+  <Hero></Hero>
+  <AnimatedTooltipPreview></AnimatedTooltipPreview>
+  <WhyChoose></WhyChoose> 
+  <OurMission></OurMission>
+</>
+ 
+);
 }
